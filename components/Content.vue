b54ee75c7cfaabc5c9e77a2fcfe7230e385c58a6
Nacho Quero <iquero@vanadis.es>
2021-10-08 13:13:09 +0200

    start base structure

---
+++ components/Content.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="
+      relative
+      flex
+      items-top
+      justify-center
+      min-h-screen
+      bg-gray-100
+      sm:items-center sm:pt-0
+    "
+  >
+    <ImgCard />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
