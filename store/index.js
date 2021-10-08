725e50cebd969a29b944bdd71b11d3431254430f
Nacho Quero <iquero@vanadis.es>
2021-10-08 13:52:29 +0200

    add state

---
+++ store/index.js
@@ -0,0 +1,19 @@
+export const state = () => ({
+  // images
+  images: [
+    {
+      id: '',
+      name: '',
+      url: '',
+    },
+  ],
+
+  // tags
+  tagToSearch: '',
+})
+
+export const mutations = {}
+
+export const actions = {}
+
+export const getters = {}
