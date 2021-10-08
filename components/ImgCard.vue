29da0ce5982ce90c4f73c785a4205a8ceff4c91d
Nacho Quero <iquero@vanadis.es>
2021-10-08 13:20:11 +0200

    make component dynamic

--- components/ImgCard.vue
+++ components/ImgCard.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg">
     <img
-      src="https:\\live.staticflickr.com/65535/51564419531_b02f943156_m.jpg"
-      alt=""
+      :src="url"
+      :alt="name"
       class="sm:rounded-t-xl h-60 w-full object-cover"
     />
     <h2 class="text-2xl leading-7 font-semibold p-6">
-      Description o Name imgage.
+      {{ name }}
     </h2>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    url: {
+      type: String,
+      default:
+        'https:\\live.staticflickr.com/65535/51564419531_b02f943156_m.jpg',
+    },
+    name: {
+      type: String,
+      default: 'no name',
+    },
+  },
+}
 </script>
 
 <style></style>
