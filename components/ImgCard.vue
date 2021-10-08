dc21eb9cf02652c46202698c2b6a7a6d8dc301a8
Nacho Quero <iquero@vanadis.es>
2021-10-08 13:13:47 +0200

    add component

---
+++ components/ImgCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg">
+    <img
+      src="https:\\live.staticflickr.com/65535/51564419531_b02f943156_m.jpg"
+      alt=""
+      class="sm:rounded-t-xl h-60 w-full object-cover"
+    />
+    <h2 class="text-2xl leading-7 font-semibold p-6">
+      Description o Name imgage.
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
