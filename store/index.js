693abcba0d50c133984c7e453441b7ddd7ef02cb
Nacho Quero <iquero@vanadis.es>
2021-10-11 09:32:26 +0200

    add mutations and actions

--- store/index.js
+++ store/index.js
@@ -1,20 +1,25 @@
 export const state = () => ({
   // images
-  images: [
-    {
-      id: '',
-      name: '',
-      url: '',
-    },
-  ],
+  images: [],
 
   // tags
   tagToSearch: '',
 })
 
-export const mutations = {}
+export const mutations = {
+  ADD_IMAGE(state, payload) {
+    state.images.push(payload)
+  },
+  SET_IMAGES(state, payload) {
+    state.images = payload
+  },
+}
 
-export const actions = {}
+export const actions = {
+  setImages({ commit }, payload) {
+    commit('SET_IMAGES', payload)
+  },
+}
 
 export const getters = {
   // get images
